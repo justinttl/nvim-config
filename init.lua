7ed1053bc02856b4883e8c4bdee9fe9512ebdd4a
justinttl <justintt.lee@gmail.com>
2024-06-06 13:21:13 -0400

    Oil file manager

--- init.lua
+++ init.lua
@@ -398,6 +398,19 @@ require('lazy').setup({
     end,
   },
 
+  -- File Explorer (Replaces netrw)
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        view_options = { show_hidden = true },
+      }
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
