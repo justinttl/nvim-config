ba973c97079777ba73e51d41223a49b03f81afac
justinttl <justintt.lee@gmail.com>
2025-07-03 22:32:31 -0400

    forward fix to lsp gd

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -22,14 +22,13 @@ return {
       }
     end,
     formatters_by_ft = {
-      -- python = function(bufnr)
-      --   if require('conform').get_formatter_info('ruff_format', bufnr).available then
-      --     return { 'ruff_format' }
-      --   else
-      --     return { 'isort', 'black' }
-      --   end
-      -- end,
-      python = { 'black', 'isort' },
+      python = function(bufnr)
+        if require('conform').get_formatter_info('ruff_format', bufnr).available then
+          return { 'ruff_format' }
+        else
+          return { 'isort', 'black' }
+        end
+      end,
       javascript = { 'prettierd' },
       typescript = { 'prettierd' },
       javascriptreact = { 'prettierd' },
